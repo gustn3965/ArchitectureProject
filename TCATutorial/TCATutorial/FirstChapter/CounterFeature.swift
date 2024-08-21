a05708c2c54f4ec2c7aced7d0754e9eef77f4bdb
gustn3965 <gustn3965>
2024-08-22 00:28:10 +0900

    TCA 튜토리얼 프로젝트 - 챕터 1 - 2 따라하기 https://pointfreeco.github.io/swift-composable-architecture/main/tutorials/composablearchitecture/01-02-addingsideeffects

--- TCATutorial/TCATutorial/FirstChapter/CounterFeature.swift
+++ TCATutorial/TCATutorial/FirstChapter/CounterFeature.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 @Reducer
 struct CounterFeature {
@@ -13,11 +14,23 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var isLoading = false
+        var fact: String? 
+        var isTimerRunning: Bool = false
     }
     
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String?)
+        case toggleTimerButtonTapped
+        case timerTrick
+    }
+    
+    
+    enum CancelID {
+        case timer
     }
     
     var body: some ReducerOf<Self> {
@@ -26,11 +39,53 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
-            case .incrementButtonTapped:
-                state.count += 1
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(data: data, encoding: .utf8)
+                    await send(.factResponse(fact))
+                }
+
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
                 
+            case .incrementButtonTapped:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                
+                if state.isTimerRunning {
+                    return .run { send in
+                        
+                        while true {
+                            
+                            
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTrick)
+                            print("")
+                        }
+                    }.cancellable(id: CancelID.timer)
+                    
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
+                
+                
+            case .timerTrick:
+                state.count += 1
+                state.fact = nil
+                
+                return .none
             }
         }
     }
